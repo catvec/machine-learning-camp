0ea26c8dbbc7d910761d1340ef54fe9637afdf47
Noah Huppert <contact@noahh.io>
2026-07-02 19:23:07 -0400

    Change format script to do rpi notebooks

--- scripts/format-notebooks.sh
+++ scripts/format-notebooks.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 readonly PROG_DIR=$(dirname $(realpath "$0"))
-readonly NOTEBOOKS_DIR="${PROG_DIR}/../notebooks"
+readonly COLAB_NOTEBOOKS_DIR="${PROG_DIR}/../notebooks"
+readonly PI_NOTEBOOKS_DIR="${PROG_DIR}/../raspberry-pi/files/notebooks"
 
 # Clean notebooks with nb-clean, preserving Colab-specific metadata (cellView, id, outputId)
-find "$NOTEBOOKS_DIR" -type f -name "*.ipynb" -exec nb-clean clean --preserve-cell-metadata colab cellView id outputId -- {} \;
+find "$COLAB_NOTEBOOKS_DIR" "$PI_NOTEBOOKS_DIR" -type f -name "*.ipynb" -exec nb-clean clean --preserve-cell-metadata colab cellView id outputId -- {} \;
